cca7083b94e74bda0917626db5039fc5fed7c802
Anas Lamiri <https://github.com/Anass-Lamiri/Client_Site>
2022-11-27 03:23:45 +0100

    making and completing the edit button

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ btn.onclick = function() {
         }
         arr.push(newTask)
         localStorage.setItem('tasks', JSON.stringify(arr) ) 
-
+        
         showData()
         clearData()
     }else {
@@ -44,11 +44,12 @@ btn.onclick = function() {
 function showData() {
     let task = ''
     for(let i=0;i<arr.length;i++) {
-        task += `<span class="task_list">
-            <p class="pa">${arr[i].text}</p>
+        task += `<span class="task_list task_list${i}">
+            <p class="pa${i}">${arr[i].text}</p>
             <div class="edit_dlt">
-                <img src="/img/edit.png" onclick='editData(${i})' class="edit" alt="edit">
+                <img src="/img/edit.png" onclick='editData(${i})' id="edit_btn${i}" class="edit" alt="edit">
                 <img src="/img/delete_icon.png" class="delete" onclick='deleteData(${i})'alt="delete">
+                <img src="/img/icon.svg" class="svg" id="end-editing${i}">
             </div>
         </span>`
     }
@@ -76,16 +77,29 @@ function elemStyle() {
 } 
 
 
-
-let edit = document.querySelector(".edit")
-
 function editData(i) {
-    
-    let test = arr[i].text
-    input.value = test
-    showData()
-    test.innerHTML = input.value
-    elemStyle()
 
+    let pa = document.querySelector(`.pa${i}`)
+    let task_list = document.querySelector(`.task_list${i}`)
+
+    let edit_btn = document.getElementById(`edit_btn${i}`);
+    let end_button = document.getElementById(`end-editing${i}`);
+    let done_btn = document.querySelector(`.svg`);
+
+
+    edit_btn.addEventListener("click", function() {
+        console.log("o");
+        pa.contentEditable = true;
+        pa.style.width = '50%'
+        task_list.style.backgroundColor = "#dddbdb";
+    });
+    end_button.addEventListener("click", function() {
+        pa.contentEditable = false;
+        task_list.style.backgroundColor = "#fff";
+
+        arr[i].text = pa.innerHTML
+        localStorage.tasks = JSON.stringify(arr)
+        console.log(pa.innerHTML);
+    })
 }
 
